d6ff13d59d211758eb220f542f36373b2833b242
OgutuJoseph <onyango.joseph@yahoo.com>
2022-06-06 15:01:14 +0300

    Updated

--- client/src/components/FileUploader/index.js
+++ client/src/components/FileUploader/index.js
@@ -1,20 +1,21 @@
 import { useState } from 'react';
 import './styles.css';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
-export const FileUploader = ({}) => {
+export const FileUploader = ({ onSuccess }) => {
 
     const [userInput, setUser] = useState({
         customer_email: '',
         customer_names: '',
         reference: ''
     })
-    const [file, setFile] = useState(null);
+    const [files, setFiles] = useState([]);
 
     const onInputChange = (e) => {
         e.persist();
         setUser({ ...userInput, [e.target.name]:e.target.value });
-        setFile(e.target.files[0])
+        setFiles(e.target.files)
     }
 
     const handleSubmit = (e) => {
@@ -22,14 +23,24 @@ export const FileUploader = ({}) => {
 
         const data = new FormData();
 
-        data.append('file', file)
+        for (let i = 0; i < files.length; i++) {
+            data.append('file', files[i])
+        }
+        
         data.append('customer_email', userInput.customer_email);
         data.append('customer_names', userInput.customer_names);
         data.append('reference', userInput.reference);
 
         axios.post('http://localhost:8000/upload', data)
-                .then((e) => { console.log('Success!') })
-                .catch((e) => { console.log('Error!') })
+                .then((response) => { 
+                    console.log('Success!')
+                    toast.success('File(s) Uploaded Successfully!')
+                    onSuccess(response.data)
+                })
+                .catch((e) => { 
+                    console.log('Error!') 
+                    toast.success('File Upload Error!')
+                })
     }
 
     return (
@@ -48,7 +59,7 @@ export const FileUploader = ({}) => {
                 <label>Reference: </label>
                 <input type="text" name="reference" value={userInput.reference} onChange={onInputChange} />
             </div>
-            <br /> 
+            <br />
             <div className="form-group files"> 
                 <label>Upload your flle</label>
                 <input 
