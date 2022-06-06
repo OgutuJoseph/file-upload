7a299d779cf9c2a13cec4edb91ab9626297ade7d
OgutuJoseph <onyango.joseph@yahoo.com>
2022-06-06 14:43:31 +0300

    Updated

--- client/src/components/FileUploader/index.js
+++ client/src/components/FileUploader/index.js
@@ -4,9 +4,15 @@ import axios from 'axios';
 
 export const FileUploader = ({}) => {
 
+    const [userInput, setUser] = useState({
+        email: '',
+        subject: ''
+    })
     const [file, setFile] = useState(null);
 
     const onInputChange = (e) => {
+        e.persist();
+        setUser({ ...userInput, [e.target.name]:e.target.value });
         setFile(e.target.files[0])
     }
 
@@ -16,14 +22,32 @@ export const FileUploader = ({}) => {
         const data = new FormData();
 
         data.append('file', file)
+        data.append('customer_email', userInput.customer_email);
+        data.append('customer_names', userInput.customer_names);
+        data.append('reference', userInput.reference);
 
-        axios.post('http://localhost:8000/upload', data)
+        axios.post('http://localhost:2000/accounting-sales/sendInvoice', data)
                 .then((e) => { console.log('Success!') })
                 .catch((e) => { console.log('Error!') })
     }
 
     return (
         <form method="post" onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Customer Email: </label>
+                <input type="text" name="customer_email" value={userInput.customer_email} onChange={onInputChange} />
+            </div>
+            <br />
+            <div className="form-group">
+                <label>Customer Names: </label>
+                <input type="text" name="customer_names" value={userInput.customer_names} onChange={onInputChange} />
+            </div>
+            <br />
+            <div className="form-group">
+                <label>Reference: </label>
+                <input type="text" name="reference" value={userInput.reference} onChange={onInputChange} />
+            </div>
+            <br /> 
             <div className="form-group files"> 
                 <label>Upload your flle</label>
                 <input 
